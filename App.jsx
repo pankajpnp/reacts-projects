d83a90632c6a01d54e0df0f522c63be5cd37713d
pankajpnp <pankajpnp122@gmail.com>
2025-05-11 20:20:34 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,35 +1,12 @@
-import React, { useState } from 'react'
-import QuizApp from './Components/QuizApp/QuizApp'
-import { Quizes } from './Components/Questions'
-import ScoreCard from './Components/QuizApp/ScoreCard'
+import React from 'react'
+import Weather from './Components/weathers/Weather'
 
 const App = () => {
-  const [submit,setsubmited]=useState(false)
-  const[selected,setselected]=useState(Array(Quizes.length).fill(-1))
-  function changeselection(val,quest){
-      let arr=selected;
-      arr[quest]=val;
-      setselected(arr)
-      console.log(arr)
-  }
-  function submitactive(){
-    console.log("Fahter Submission")
-    setsubmited(true)
-    
-  }
-  function retake(){
-    setsubmited(false)
-    reset()
-  }
-  function reset(){
-    setselected(Array(Quizes.length).fill(-1))
-  }
   return (
     <>
-    {submit ?<ScoreCard arr={selected} retake={retake} />:<QuizApp changeselection={changeselection} selected={selected} resetarr={reset} submited={submitactive}/> }
-    
+    <Weather/>
     </>
-
   )
 }
+
 export default App
